3172f5e2e6aabbaf7b2b23e9d3ea05940c66ec36
Kerry Kline <kerry@bnecreative.com>
2018-08-01 16:53:29 -0700

    Update bne_cpt.php

--- bne_cpt.php
+++ bne_cpt.php
@@ -9,7 +9,7 @@
  * 	@version 	1.0.2
  * 	License: 	GPL2
  *
- *	Copyright 2015 BNE Creative
+ *	Copyright 2018 BNE Creative
  *
  *	This program is free software; you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License version 2,
